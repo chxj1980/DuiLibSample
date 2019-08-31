cd95382930ca4921865d076edc39142473e69cce
TaoXing <taoxing@mezhi.com.cn>
2019-08-31 15:28:36 +0800

    增加duilib例子，helloworld

---
+++ DuiLibHelloWorld/main.cpp
@@ -0,0 +1,17 @@
+
+#include <UIlib.h>
+#include <Windows.h>
+
+#include "CDuiFrameWnd.h"
+
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+	DuiLib::CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+
+	return EXIT_SUCCESS;
+}
