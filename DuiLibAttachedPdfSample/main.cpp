1f50e426acaf6d280db4be0f5380dfdc5c8b660b
Gooooodman <taoxing2113@foxmail.com>
2019-09-02 17:18:56 +0800

    DuiLib入门文档例子更新

---
+++ DuiLibAttachedPdfSample/main.cpp
@@ -0,0 +1,9 @@
+
+#include <Windows.h>
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+
+
+	return EXIT_SUCCESS;
+}
