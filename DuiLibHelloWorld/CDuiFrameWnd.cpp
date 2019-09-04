0c492b43b684f6cdcd7cc5b44af969df977c216b
TaoXing <taoxing@mezhi.com.cn>
2019-09-04 23:29:21 +0800

    HelloWorld例子增加行为

--- DuiLibHelloWorld/CDuiFrameWnd.cpp
+++ DuiLibHelloWorld/CDuiFrameWnd.cpp
@@ -7,7 +7,13 @@ LPCTSTR CDuiFrameWnd::GetWindowClassName() const
 
 void CDuiFrameWnd::Notify(DuiLib::TNotifyUI & msg)
 {
-	//
+	if (msg.sType == _T("click"))
+	{
+		if (msg.pSender->GetName() == _T("btnHello"))
+		{
+			MessageBox(NULL, _T("woshianniu"), _T("dianjileanniu"), NULL);
+		}
+	}
 }
 
 LRESULT CDuiFrameWnd::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -17,12 +23,29 @@ LRESULT CDuiFrameWnd::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	if (uMsg == WM_CREATE)
 	{
 		DuiLib::CControlUI *pWnd = new DuiLib::CButtonUI;
+		pWnd->SetName("btnHello");
 		pWnd->SetText(_T("Hello World"));   // 设置文字
 		pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
 
 		m_PaintManager.Init(m_hWnd);
 		m_PaintManager.AttachDialog(pWnd);
+		m_PaintManager.AddNotifier(this);
 		return lRes;
+	}
+	else if (uMsg == WM_NCACTIVATE)
+	{
+		if (!IsIconic(m_hWnd))
+		{
+			return (wParam == 0) ? TRUE : FALSE;
+		}
+	}
+	else if (uMsg == WM_NCCALCSIZE)
+	{
+		return 0;
+	}
+	else if (uMsg == WM_NCPAINT)
+	{
+		return 0;
 	}
 
 	if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
